46211ee3651f5b16b911623c29698b5d6bab4fa3
duongnq2798 <duongnq27.98@gmail.com>
2020-07-27 14:14:26 +0700

    Create Frontend Layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,16 +1,25 @@
-@extends('layouts.app')
+@extends('layouts.frontend.app')
+@section('title','Login')
+
+@push('css')
+<link href="/assets/frontend/css/auth/styles.css" rel="stylesheet">
+<link href="/assets/frontend/css/auth/responsive.css" rel="stylesheet">
+@endpush
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
 
-                <div class="card-body">
+<div class="slider display-table center-text">
+    <h1 class="title display-table-cell"><b>LOGIN</b></h1>
+</div><!-- slider -->
+
+<section class="blog-area section">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-2 col-md-0"></div>
+            <div class="col-lg-8 col-md-12">
+                <div class="post-wrapper">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
-
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
@@ -24,7 +33,6 @@
                                 @enderror
                             </div>
                         </div>
-
                         <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
@@ -38,7 +46,6 @@
                                 @enderror
                             </div>
                         </div>
-
                         <div class="form-group row">
                             <div class="col-md-6 offset-md-4">
                                 <div class="form-check">
@@ -50,7 +57,6 @@
                                 </div>
                             </div>
                         </div>
-
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
@@ -65,9 +71,11 @@
                             </div>
                         </div>
                     </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+                </div><!-- post-wrapper -->
+            </div><!-- col-sm-8 col-sm-offset-2 -->
+        </div><!-- row -->
+
+    </div><!-- container -->
+</section><!-- section -->
 @endsection
+
